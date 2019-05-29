4838390d6fb2f28ae4902a9131a30f0c71829bcc
Victor Pragana <victormerigue@gmail.com>
2019-05-29 15:05:52 -0300

    create all rest routes and add cors

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // iniciando o app
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 
 requireDir('./src/models');
-
-// const Product = mongoose.model('product');
 
 // rotas
 app.use('/api', require("./src/routes"));
